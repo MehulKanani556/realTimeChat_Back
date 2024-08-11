de94ffbeeff3080ff344a7b7d2fc31171e4e63d5
Mehul Kanani <mkanani556@gmail.com>
2024-08-11 18:12:30 +0530

    sa

--- api/socket.js
+++ api/socket.js
@@ -2,14 +2,11 @@ const { Server } = require("socket.io");
 
 const ioHandler = (req, res) => {
   if (!res.socket.server.io) {
+    console.log("*First use, starting socket.io");
     const io = new Server(res.socket.server, {
-        cors: {
-          origin: "*",
-          methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-          allowedHeaders: ["Content-Type", "Authorization"],
-          credentials: true
-        }
-      });
+      path: "/api/socketio",
+      addTrailingSlash: false,
+    });
 
     io.on("connection", (socket) => {
       console.log("User connected", socket.id);
@@ -17,11 +14,13 @@ const ioHandler = (req, res) => {
       socket.broadcast.emit("test", "Good Morning ...");
 
       socket.on('message', (data) => {
-        io.to(data.id).emit('msg', data);
+        io.emit('msg', data);
       });
     });
 
     res.socket.server.io = io;
+  } else {
+    console.log("socket.io already running");
   }
   res.end();
 };
