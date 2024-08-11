04b846b6ed70d803dc787cb054c69ad168a36504
Mehul Kanani <mkanani556@gmail.com>
2024-08-11 15:32:45 +0530

    socket

---
+++ api/socket.js
@@ -0,0 +1,27 @@
+const { Server } = require("socket.io");
+
+const ioHandler = (req, res) => {
+  if (!res.socket.server.io) {
+    const io = new Server(res.socket.server, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+      }
+    });
+
+    io.on("connection", (socket) => {
+      console.log("User connected", socket.id);
+
+      socket.broadcast.emit("test", "Good Morning ...");
+
+      socket.on('message', (data) => {
+        io.to(data.id).emit('msg', data);
+      });
+    });
+
+    res.socket.server.io = io;
+  }
+  res.end();
+};
+
+module.exports = ioHandler;
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const connectChat = require('./socketIo');
+// const connectChat = require('./socketIo');
 
 var app = express();
 // connectChat();
