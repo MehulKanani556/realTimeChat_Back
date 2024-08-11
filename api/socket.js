5672d8e498a8b3074ac0503d7feac3a1deb7a194
Mehul Kanani <mkanani556@gmail.com>
2024-08-11 18:00:47 +0530

    soket

--- api/socket.js
+++ api/socket.js
@@ -3,11 +3,13 @@ const { Server } = require("socket.io");
 const ioHandler = (req, res) => {
   if (!res.socket.server.io) {
     const io = new Server(res.socket.server, {
-      cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-      }
-    });
+        cors: {
+          origin: "*",
+          methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+          allowedHeaders: ["Content-Type", "Authorization"],
+          credentials: true
+        }
+      });
 
     io.on("connection", (socket) => {
       console.log("User connected", socket.id);
@@ -24,4 +26,4 @@ const ioHandler = (req, res) => {
   res.end();
 };
 
-module.exports = ioHandler;
+module.exports = ioHandler
